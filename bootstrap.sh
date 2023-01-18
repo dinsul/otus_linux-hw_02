3d5d528e2ca44a2b12786488dac97c07324aa109
Dinsul <dinsul@rambler.ru>
2023-01-18 13:16:44 +0300

    add results of runnig and description in readme

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,4 +3,13 @@
 mkdir -p ~root/.ssh
 cp ~vagrant/.ssh/auth* ~root/.ssh
 yum install -y mdadm smartmontools hdparm gdisk
+
+echo "####### Проверяем количество дисков в системе #######"
+if [[ `ls /dev/sd? | wc -l` == 6 ]]; then 
+    echo "    count of disks is well"
+else
+    echo "Wrong disks count"
+    exit 1
+fi
+
 echo "Welcome!!!"
